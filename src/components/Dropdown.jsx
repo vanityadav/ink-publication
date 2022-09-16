fcdb7f15a2827d9efd8dd45e9ee8ea4f961a0ec9
vanityadav <vanityadav08@gmail.com>
2022-09-16 22:26:02 +0530

    Update Dropdown.jsx

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect } from "react";
+import { useState, useRef } from "react";
 
 export default function Dropdown() {
   const [dropdownActive, setDropdownActive] = useState(false);
   const [selected, setSecected] = useState();
+  const dropRef = useRef();
+
+  useEffect(() => {
+    function handleDropdown(event) {
+      console.log(dropRef.current);
+      console.log(selected);
+      console.log(dropdownActive);
+      console.log(event.target);
+      if (event.target !== dropRef.current) setDropdownActive(false);
+    }
+
+    document.addEventListener("click", handleDropdown);
+
+    return () => {
+      document.removeEventListener("click", handleDropdown);
+    };
+  });
 
   const items = ["array", "react", "hello", "item"];
   return (
     <div className="dropdown">
       <div
+        ref={dropRef}
         onClick={() => setDropdownActive(!dropdownActive)}
         className="dropdown-button"
       >
